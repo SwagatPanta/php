e57c8b57a07ae5261d4608b694fd6ddd794c74cc
SwagatPanta <panta.ragataj@gmail.com>
2016-05-09 19:14:25 +0545

    third commit

---
+++ datentime.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "today is ".date("yy/mm/dd")."<br>";
+echo "today is ".date("y.m.d")."<br>";
+echo "today is ".date("y-m-d")."<br>";
+echo "today is ".date("l")."<br>";
+echo "today is ".date("m")."<br>";
+echo "today is ".date("y")."<br>";
+echo "today is ".date("YY/MM/DD")."<br>";
+echo "today is ".date("Y.M.D")."<br>";
+echo "today is ".date("Y-M-D")."<br>";
+echo "today is ".date("D/M/Y/")."<br>";
+echo "now ".date("h:i:sa")."<br>";
+echo "now ".date("h:i:sa")."<br>";
+date_default_timezone_set("Asia/kathmandu")."<br>";
+echo date("h:i:sa");
+?>
---
+++ flicker.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+$('button').click(function(){
+
+	$("button").removeClass("selected");
+	$(this).addClass("selected");
+	var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+	var animal = $(this).text();
+	var flickerOptions={
+		//javascript object
+		tags:animal,
+		format:"json"
+	};
+
+	function displayPhotos(data){
+var photoHtml='<ul>';
+$.each(data.items,function(i,photo){
+
+	photoHtml+='<li class="grid-25 table-grid-50">';
+	photoHtml+='<a href="'+photo.link+'" class="image">';
+	photoHtml+='<img src="'+photo.media.m+'"></a></li>';
+});
+photoHtml+='</ul>';
+$('#photos').html(photoHtml);
+	};//displayPhotos end
+	$.getJSON(flickerAPI,flickerOptions,displayPhotos);
+});//clieck end
+
+});//ready end
